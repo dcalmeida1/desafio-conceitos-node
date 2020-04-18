83bea4eec990e73b374ee27ccdec17f948d9a494
Diego Cadena <diegoepc@gmail.com>
2020-04-18 15:56:48 -0300

    deletar

--- src/app.js
+++ src/app.js
@@ -67,7 +67,17 @@ app.put("/repositories/:id", validateRepositoryId, (request, response) => {
 });
 
 app.delete("/repositories/:id", validateRepositoryId, (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex((respository) => respository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+
+  repositores = repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", validateRepositoryId, (request, response) => {
